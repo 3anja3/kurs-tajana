bb02583de0e442028409d333a620409086e64e5e
3anja3 <147884551+3anja3@users.noreply.github.com>
2024-04-15 20:28:06 +0200

    addes files 2

---
+++ src/components/image.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Image = ({ largeImage, smallImage }) => {
+  return (
+    <div className="portfolio-item">
+      <div className="hover-bg">
+        {" "}
+        <a href={largeImage} data-lightbox-gallery="gallery1">
+          <div className="hover-text">
+          </div>
+          <img src={smallImage} className="img-responsive"/>{" "}
+        </a>{" "}
+      </div>
+    </div>
+  );
+};
